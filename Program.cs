550d349ffad95210a76df1b6db21fe1e31b8a525
huynq1992hpvn <huynq1992hpvn@gmail.com>
2023-06-05 01:01:06 +0700

    lan1

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classhinhchunhat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("moi ban nhap chieu rong ");
+            int width = int.Parse(Console.ReadLine());
+            Console.WriteLine(" moi ban nhap chieu cao ");
+            int height = int.Parse(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(width, height);
+            Console.WriteLine("thong so hinh chu nhat " + rectangle.Display());
+            Console.WriteLine("dien tich la: " + rectangle.getArea());
+            Console.WriteLine("Chu vi la: " + rectangle.getPer());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classhinhchunhat
+{
+    class Rectangle
+    {
+        private int width;
+        private int height;
+
+        public Rectangle(int width = 1, int height = 1)
+        {
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public int Width { get => width; set => width = value; }
+        public int Height { get => height; set => height = value; }
+        public int getArea() { return width * height; }
+        public int getPer() { return 2 * (width + height); }
+        public string Display()
+        {
+            return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+
+        }
+    }
+}
